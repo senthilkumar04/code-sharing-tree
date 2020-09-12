ee4f145756e54a600ea3c1c5edb9bde837e0a4a4
Senthilkumar Kalyanasundaram <senthilkumar.cse04@gmail.com>
2020-09-12 13:19:17 +1000

    chore: removed identity widget from document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,6 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         </Head>
         <body>
           <Main />
